499d8d91030dfc1dd71eb57e7748d92aaf290cce
ffniners <codycusimano@yahoo.com>
2024-02-03 19:44:07 -0800

    Worked on front end api call.

--- foodfinder-frontend/src/pages/UiPage/UiPage.jsx
+++ foodfinder-frontend/src/pages/UiPage/UiPage.jsx
@@ -1,14 +1,20 @@
 import React from 'react'; 
+import axios from 'axios'; 
 import { Container } from '@mui/material/Container'; 
 import Box from '@mui/material/Box'; 
 import Navbar from '../../components/Navbar/Navbar';
 import BottomBar from '../../components/BottomBar/BottomBar';
 
-
+const fetchData = async (longitude, latitude) => {
+const response = await axios.get("https://api.yelp.com/v3/businesses/search?latitude=90&longitude=180&term=restaurants&radius=8045&categories=french&categories=bars&sort_by=distance&limit=50");
+return response.data
+}
 
 
 const UiPage = () => {
-
+    
+   
+   
  return(
     <Container maxWidth={"sm"} alignItems="center" disableGutter={"true"} sx={{padding: "0 5px 0 5px"}}>
         <Navbar />
