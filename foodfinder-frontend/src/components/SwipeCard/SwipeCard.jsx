b60ce089870099080024701fdc6d8d80f6cd62ca
ffniners <codycusimano@yahoo.com>
2024-02-04 18:24:07 -0800

    fix

--- foodfinder-frontend/src/components/SwipeCard/SwipeCard.jsx
+++ foodfinder-frontend/src/components/SwipeCard/SwipeCard.jsx
@@ -17,42 +17,31 @@ const SwipeCard = ({data}) => {
    
    return (
     <Box height="80%" width="100%" borderRadius="11"> 
-    
-  {data.businesses.map((business) => (
-        <TinderCard onSwipe={onSwipe} onCardLeftScreen={() => onCardLeftScreen('Gone')} style={{borderRadius:"11"}}>
-            <div style={{backgroundImage: `url(${business.image_url})`, 
-                         backgroundRepeat: "no-repeat", 
-                         backgroundSize: "cover", 
-                         width: "100%", 
-                         height: "100%", 
-                         display: "flex", 
-                         flexDirection:"column", 
-                         alignItems: "flex-end",
-                         borderRadius:"11"}}>
-                <Box width="100%" height="10%">
-                </Box>
-                <Box width="100%" display="flex" flexDirection="column">
-                 <Typography variant="h2" align='left' sx={{fontFamily: 'Mulish, sans-serif', color:"white"}}>
-                        {business[0].alias}
-                 </Typography>
-                 <Typography variant="p" align='left' sx={{fontFamily: 'Mulish, sans-serif',  color:"white"}}>
-                    {business[0].categories[0].title}
-                    
-                 </Typography>
-                 <Typography variant="p" align='left' sx={{fontFamily: 'Mulish, sans-serif',  color:"white"}}>
-                     {business[0].distance}
-                 </Typography>
-                </Box>
-            </div>
-        </TinderCard>
-        ))
-        }
+    {
+  data.map((group) => ( // Iterate over each group in the data array
+    group.businesses.map((business) => ( // Iterate over each business in the businesses array
+    <TinderCard key={business.id} onSwipe={onSwipe} onCardLeftScreen={() => onCardLeftScreen('Gone')} style={{borderRadius:"11"}}>
+      <div > // Use business.id as the key for each item
+        <h2>{business.name}</h2>
+        <img src={business.image_url} alt={business.name} />
+        <p>{business.display_phone}</p>
+        <p>{business.location.address1} {business.location.city}</p>
+        <p>{business.categories.map(category => category.title).join(', ')}</p>
+        <p>{business.distance.toFixed(2)} meters away</p>
+        {/* ... add any other details you want from the business object */}
+      </div>
+     </TinderCard>
+    ))
+  ))
+}
+
+  
     </Box>
             )
 }
 
 
-export default SwipeCard; 
+export default SwipeCard;
 
 
 /*
