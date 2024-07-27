cf9e12635094183a4753324d85624dd3aebef28e
Thomas Abadines <thomas.abadines@gmail.com>
2024-07-26 17:34:46 -0700

    Fixed Types

--- foodfinder-workers/foodfinder/src/model.ts
+++ foodfinder-workers/foodfinder/src/model.ts
@@ -1,33 +1,26 @@
-const PREFIX = 'v1:post:'
-
-declare global {
-  interface Crypto {
-    randomUUID(): string
-  }
+interface Category {
+  title: string;
 }
 
-export interface Post {
-  id: string
-  title: string
-  body: string
+interface Location {
+  display_address: string[];
 }
 
-//export interface User {
-//  id: string
-//  first_name: string
-//  body: string
-//}
-
-export type Param = {
-  title: string
-  body: string
+interface Business {
+  id: string;
+  name: string;
+  image_url: string;
+  categories: Category[];
+  rating: number;
+  price: string;
+  location: Location;
 }
 
-const generateID = (key: string) => {
-  return `${PREFIX}${key}`
+interface ApiResponse {
+  businesses: Business[];
 }
 
-export const fetchYelpData = async (latitude: string, longitude: string, yelpApiKey: string): any => {
+export const fetchYelpData = async (latitude: string, longitude: string, yelpApiKey: string): Promise<any> => {
   const options = {
       method: 'GET',
       headers: {
@@ -37,10 +30,10 @@ export const fetchYelpData = async (latitude: string, longitude: string, yelpApi
   };
 
   if(latitude && longitude) {
-        let simplifiedList = []
+        let simplifiedList: any[] = []
       try {
           let res = await fetch(`https://api.yelp.com/v3/businesses/search?open_now=true&sort_by=distance&latitude=${latitude}&longitude=${longitude}&term=food&categories=restaurants&limit=50`, options);
-          let data = await res.json();
+          let data: ApiResponse = await res.json();
             console.log(data)
             console.log("latitude = " + latitude + " longitude = " + longitude)
           simplifiedList = await data.businesses?.map(({ id, name, image_url, categories, rating, price, location }) => (
@@ -67,51 +60,35 @@ export const fetchYelpData = async (latitude: string, longitude: string, yelpApi
   }
 }
 
-export const getPosts = async (KV: KVNamespace): Promise<Post[]> => {
-  const list = await KV.list({ prefix: PREFIX })
-  const keys = list.keys
-  const posts: Post[] = []
 
-  const len = keys.length
-  for (let i = 0; i < len; i++) {
-    const value = await KV.get(keys[i].name)
-    if (value) {
-      const post: Post = JSON.parse(value)
-      posts.push(post)
-    }
-  }
+// export const getPost = async (KV: KVNamespace, id: string): Promise<Post | undefined> => {
+//   const value = await KV.get(generateID(id))
+//   if (!value) return
+//   const post: Post = JSON.parse(value)
+//   return post
+// }
 
-  return posts
-}
+// export const createPost = async (KV: KVNamespace, param: Param): Promise<Post | undefined> => {
+//   if (!(param && param.title && param.body)) return
+//   const id = crypto.randomUUID()
+//   const newPost: Post = { id: id, title: param.title, body: param.body }
+//   await KV.put(generateID(id), JSON.stringify(newPost))
+//   return newPost
+// }
 
-export const getPost = async (KV: KVNamespace, id: string): Promise<Post | undefined> => {
-  const value = await KV.get(generateID(id))
-  if (!value) return
-  const post: Post = JSON.parse(value)
-  return post
-}
+// export const updatePost = async (KV: KVNamespace, id: string, param: Param): Promise<boolean> => {
+//   if (!(param && param.title && param.body)) return false
+//   const post = await getPost(KV, id)
+//   if (!post) return false
+//   post.title = param.title
+//   post.body = param.body
+//   await KV.put(generateID(id), JSON.stringify(post))
+//   return true
+// }
 
-export const createPost = async (KV: KVNamespace, param: Param): Promise<Post | undefined> => {
-  if (!(param && param.title && param.body)) return
-  const id = crypto.randomUUID()
-  const newPost: Post = { id: id, title: param.title, body: param.body }
-  await KV.put(generateID(id), JSON.stringify(newPost))
-  return newPost
-}
-
-export const updatePost = async (KV: KVNamespace, id: string, param: Param): Promise<boolean> => {
-  if (!(param && param.title && param.body)) return false
-  const post = await getPost(KV, id)
-  if (!post) return false
-  post.title = param.title
-  post.body = param.body
-  await KV.put(generateID(id), JSON.stringify(post))
-  return true
-}
-
-export const deletePost = async (KV: KVNamespace, id: string): Promise<boolean> => {
-  const post = await getPost(KV, id)
-  if (!post) return false
-  await KV.delete(generateID(id))
-  return true
-}
+// export const deletePost = async (KV: KVNamespace, id: string): Promise<boolean> => {
+//   const post = await getPost(KV, id)
+//   if (!post) return false
+//   await KV.delete(generateID(id))
+//   return true
+// }
